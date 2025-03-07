63e90ea9ca9f55a0fe14e458450d7cafa009dad5
Mathieu Morelle <mathieu@minedeprod.com>
2025-03-07 16:37:19 +0100

    1637

--- next.config.js
+++ next.config.js
@@ -2,14 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'localhost', 'www.culturecontreculture.fr'],
+    domains: ['localhost', 'www.culturecontreculture.fr'],
   },
   experimental: {
     serverActions: true,
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: 'https://cciypmugbemibaeuppco.supabase.co',
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjaXlwbXVnYmVtaWJhZXVwcGNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1NzE3NjEsImV4cCI6MjA0MjE0Nzc2MX0.Jz6v2Akm17_xtPw14w6RaaPV4oQ-dEGpqJhJxEd2BCo',
   }
 };
 
