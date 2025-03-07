cdbdf07a96dc60ca82d0b9ff9c309232ff8acbf9
Mathieu Morelle <mathieu@minedeprod.com>
2025-03-07 15:31:38 +0100

    1524

--- next.config.js
+++ next.config.js
@@ -10,24 +10,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: 'https://cciypmugbemibaeuppco.supabase.co',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjaXlwbXVnYmVtaWJhZXVwcGNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1NzE3NjEsImV4cCI6MjA0MjE0Nzc2MX0.Jz6v2Akm17_xtPw14w6RaaPV4oQ-dEGpqJhJxEd2BCo',
-  },
+  }
 };
 
-
-module.exports = {
-  // Vos configurations existantes...
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/app/page',
-        permanent: true,
-      },
-      {
-        source: '/achats',
-        destination: '/app/achats/page',
-        permanent: true,
-      },
-    ]
-  },
-}
+module.exports = nextConfig;
